198d81d12096028c2f0ae0c613a1011a818c7310
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-21 23:27:32 +0200

    create createBricks function and run it

--- game.js
+++ game.js
@@ -152,6 +152,24 @@ const brick = {
 
 let bricks = [];
 
+// create bricks function
+function createBricks() {
+  for (let r = 0; r < brick.row; r++) {
+    bricks[r] = [];
+    for (let c = 0; c < brick.column; c++) {
+      bricks[r][c] = {
+        x: c * (brick.offSetLeft + brick.width) + brick.offSetLeft,
+        y:
+          r * (brick.offSetTop + brick.height) +
+          brick.offSetTop +
+          brick.marginTop,
+        status: true
+      };
+    }
+  }
+}
+createBricks();
+
 // draw function
 function draw() {
   drawPaddle();
